f025570b36cdbd672f92bd6611a906c86b1f2f7b
SidhantNaik <mr.sidhantnaik@gmail.com>
2025-02-14 23:12:50 +0530

    Implemented the code to store user feedback to the Feedback table.
    A new table named `Feedback` has been created in the User database. The code to accept data from `Feedback.html` and save it to the `Feedback` table has been implemented.

--- User/views.py
+++ User/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, HttpResponse
+from User.models import Feedback
 
 def index(request):
     return render (request, 'Index.html')
 
 
 def feedback(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        description = request.POST['description']
+
+        feedback = Feedback(name=name, email=email, phone=phone, description=description)
+        feedback.save()
     return render (request, 'Feedback.html')
 
 
